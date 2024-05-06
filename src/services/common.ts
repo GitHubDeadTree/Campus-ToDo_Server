0932f7e89b02068c83a150b7aaf407770843dcb7
Wang Bin <2799400448@qq.com>
2024-05-06 14:42:39 +0800

    在‘main’提交

--- src/services/common.ts
+++
@@ -1,8 +0,0 @@
-import http from '@/utils/http'
-
-// 获取后台首页面板数据
-export function serviceGetPanelData(params?: object) {
-  return http.get('/panel', { params })
-}
-
-
--- src/services/system.ts
+++
@@ -1,6 +0,0 @@
-import http from '@/utils/http'
-
-// 获取系统信息
-export function serviceGetSystemInfo() {
-  return http.get('/system/info')
-}
